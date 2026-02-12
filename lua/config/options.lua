980310b8db9b4253d3610e7f94d7bbf1c54ad473
neptali <njpm28@gmail.com>
2026-02-12 19:11:28 -0400

    init

---
+++ lua/config/options.lua
@@ -0,0 +1,8 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+--
+--
+vim.g.python3_host_prog = "/home/neptali/miniconda3/bin/python3" -- set default python env
+vim.g.loaded_perl_provider = 0 -- fix warning to  load perl
+vim.g.loaded_ruby_provider = 0 -- fix warning to  load ruby
