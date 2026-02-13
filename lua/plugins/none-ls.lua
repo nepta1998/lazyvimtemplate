e5ee9915c2527722e9af625e408bd56ceac72e5b
neptali <njpm28@gmail.com>
2026-02-12 22:21:35 -0400

    change none ls config

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,17 @@
+return {
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim", -- Indispensable para flake8
+  },
+  opts = function(_, opts)
+    local nls = require("null-ls")
+    opts.root_dir = opts.root_dir
+      or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
+
+    opts.sources = vim.list_extend(opts.sources or {}, {
+      -- Usamos la extensión externa en lugar del builtin antiguo
+      require("none-ls.diagnostics.flake8"),
+      nls.builtins.formatting.black,
+    })
+  end,
+}
