9c321c55d83e19112420bfb51f67329b9b11cd6c
neptali <njpm28@gmail.com>
2026-02-12 21:07:51 -0400

    adding extras

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,4 @@
 vim.g.python3_host_prog = "/home/neptali/miniconda3/bin/python3" -- set default python env
 vim.g.loaded_perl_provider = 0 -- fix warning to  load perl
 vim.g.loaded_ruby_provider = 0 -- fix warning to  load ruby
+vim.g.lazyvim_python_lsp = "basedpyright"
