e12e2cc9b7e2c0800499e352ebd5580be3a96e54
neptali <njpm28@gmail.com>
2026-02-21 18:10:01 -0400

    comment

--- lua/plugins/kanagawa-blur.lua
+++ lua/plugins/kanagawa-blur.lua
@@ -2,4 +2,10 @@ return {
   "Gentleman-Programming/gentleman-kanagawa-blur",
   name = "gentleman-kanagawa-blur",
   priority = 1000,
+  -- opts = {
+  --   variant = "sakura_night_blur",
+  --   styles = {
+  --     booleans = { italic = true, bold = true },
+  --   },
+  -- },
 }
