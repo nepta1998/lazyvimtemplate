21f342c3a0e40a11b1629b0102bce7f849b30316
neptali <njpm28@gmail.com>
2026-02-21 19:42:15 -0400

    adding toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      -- Set terminal options here (e.g., direction = "float")
+    },
+    keys = {
+      -- Example: Press <leader>tf to open a floating terminal
+      { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle Floating Terminal" },
+      -- Example: Press <leader>th to open a horizontal terminal
+      { "<leader>th", "<cmd>ToggleTerm size=10 direction=horizontal<cr>", desc = "Toggle Horizontal Terminal" },
+      -- Example: Overwrite default toggle key (e.g., to <C-/>)
+      { "<leader>tt", "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal" },
+    },
+  },
+}
