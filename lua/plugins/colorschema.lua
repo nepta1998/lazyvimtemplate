45713921fcfc133e672ad2d279a7527d886aa00d
neptali <njpm28@gmail.com>
2026-02-24 10:46:19 -0400

    adding copilot and transparent bg

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -12,25 +12,23 @@ return {
       -- },
       highlight_overrides = {
         -- Cambia el valor de bg a "none" para que sea transparente
-        -- LspReferenceText = { bg = "none", underline = true },
-        -- LspReferenceRead = { bg = "none", underline = true },
-        -- LspReferenceWrite = { bg = "none", underline = true },
+        LspReferenceText = { bg = "none", underline = true },
+        LspReferenceRead = { bg = "none", underline = true },
+        LspReferenceWrite = { bg = "none", underline = true },
 
         -- Resaltado de búsqueda (estos ya los tienes, asegúrate que no tengan conflictos)
-        -- Search = { bg = "#2d4f67" },
-        -- CurSearch = { bg = "#7e9cd8" },
+        Search = { bg = "none" },
+        CurSearch = { bg = "none" },
 
         IlluminatedWordText = { bg = "none", underline = true },
         IlluminatedWordRead = { bg = "none", underline = true },
         IlluminatedWordWrite = { bg = "none", underline = true },
-        NormalFloat = { bg = "none" }, -- All floating buffers background like the lsp, autocomplete and such
-        FloatBorder = { bg = "none" }, -- Most floating borders except telescope
         TelescopeBorder = { bg = "none" },
         PopMenu = { bg = "none", blend = 0 },
 
-        -- SnacksExplorerNormal = { bg = "none", underline = true },
-        -- NormalFloat = { bg = "none", underline = true },
-        -- FloatBorder = { bg = "none", underline = true },
+        SnacksExplorerNormal = { bg = "none" },
+        NormalFloat = { bg = "none" },
+        FloatBorder = { bg = "none" },
       },
     },
   },
