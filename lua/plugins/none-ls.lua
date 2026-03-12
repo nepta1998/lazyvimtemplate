f69a5cfef15ae4bb26aa3395aaf68ae219129d1f
neptali <njpm28@gmail.com>
2026-03-12 19:27:20 -0400

    fix error

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,24 +1,33 @@
+
 return {
   "nvimtools/none-ls.nvim",
   dependencies = {
-    "nvimtools/none-ls-extras.nvim", -- Indispensable para flake8
+    "nvimtools/none-ls-extras.nvim",
   },
   opts = function(_, opts)
     local nls = require("null-ls")
+    
+    -- 1. Aseguramos el root_dir
     opts.root_dir = opts.root_dir
       or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
 
-    opts.sources = vim.list_extend(opts.sources or {}, {
-      -- Usamos la extensión externa en lugar del builtin antiguo
+    -- 2. Definimos las fuentes (limpiando las problemáticas)
+    local sources = {
       require("none-ls.diagnostics.flake8"),
       nls.builtins.formatting.black,
       nls.builtins.formatting.isort,
-    })
-    if opts.sources then
-      -- Filtramos las fuentes para eliminar fish_indent y fish (diagnostics)
-      opts.sources = vim.tbl_filter(function(source)
-        return not (source.name:find("fish"))
-      end, opts.sources)
-    end
+      -- Añadimos Prettier para Markdown pero evitamos el linter que crashea
+      nls.builtins.formatting.prettierd,
+    }
+
+    -- 3. Combinamos con las fuentes que LazyVim ya trae
+    opts.sources = vim.list_extend(opts.sources or {}, sources)
+
+    -- 4. FILTRO CRÍTICO: Eliminamos lo que da error (fish y markdownlint)
+    opts.sources = vim.tbl_filter(function(source)
+      -- Quitamos fish y el linter de markdown que causa el "index out of range"
+      return not (source.name:find("fish") or source.name:find("markdownlint"))
+    end, opts.sources)
   end,
 }
+
