433f06b108a04aad656d8a9a4e6df6c54975025c
neptali <njpm28@gmail.com>
2026-03-13 10:23:40 -0400

    adding isort and black

--- lua/plugins/coform.lua
+++ lua/plugins/coform.lua
@@ -1,10 +1,12 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    opts = function(_, opts)
-      -- Eliminar fish_indent de la lista de formateadores disponibles
-      opts.formatters_by_ft = opts.formatters_by_ft or {}
-      opts.formatters_by_ft.fish = nil
-    end,
-  },
+  "stevearc/conform.nvim",
+  opts = function(_, opts)
+    opts.formatters_by_ft = opts.formatters_by_ft or {}
+
+    -- Configurar isort para Python (puedes añadir "black" después si quieres)
+    opts.formatters_by_ft.python = { "isort", "black" }
+
+    -- Mantener tu ajuste para fish
+    opts.formatters_by_ft.fish = nil
+  end,
 }
