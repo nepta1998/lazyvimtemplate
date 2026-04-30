6c081da99a0c6f30d03d70b7ec6dc903d0d07fde
neptali <njpm28@gmail.com>
2026-04-30 00:39:00 -0400

    fix conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,12 +1,36 @@
+-- lua/plugins/conform.lua (o lint.lua, elige uno solo para ambos)
 return {
-  "stevearc/conform.nvim",
-  opts = function(_, opts)
-    opts.formatters_by_ft = opts.formatters_by_ft or {}
+  -- 1. CONFORM: Formateo con black e isort
+  {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile", "BufWritePost" },
+    opts = {
+      formatters_by_ft = {
+        python = { "isort", "black" },
+        fish = nil,
+      },
+      -- Ejecutar formateo al guardar
+      -- format_on_save = {
+      --   timeout_ms = 500,
+      --   lsp_fallback = true,
+      -- },
+    },
+    -- config = function(_, opts)
+    --   require("conform").setup(opts)
+    -- end,
+  },
 
-    -- Configurar isort para Python (puedes añadir "black" después si quieres)
-    opts.formatters_by_ft.python = { "isort", "black" }
-
-    -- Mantener tu ajuste para fish
-    opts.formatters_by_ft.fish = nil
-  end,
+  -- 2. NVIM-LINT: Linting con flake8
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      -- Herramientas que SOLO INFORMAN (diagnósticos)
+      linters_by_ft = {
+        python = { "flake8" },
+        dockerfile = { "trivy" },
+        -- Puedes añadir trivy a otros si quieres
+        terraform = { "trivy" },
+      },
+    },
+  },
 }
