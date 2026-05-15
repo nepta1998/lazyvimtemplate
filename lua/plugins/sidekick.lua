19770fec800db6ad42d37fcc1285793a118b433b
neptali <njpm28@gmail.com>
2026-05-15 10:16:30 -0400

    adding kilo code

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -4,5 +4,24 @@ return {
     nes = {
       enabled = false, -- Esto desactiva las sugerencias de edición en el texto
     },
+    cli = {
+      mux = {
+        enabled = true,
+        backend = "tmux", -- or "zellij"
+      },
+      tools = {
+        kilo = {
+          cmd = { "kilo" },
+          -- keys = {
+          --   submit = {
+          --     "<c-s>",
+          --     function(t)
+          --       t:send("\n")
+          --     end,
+          --   },
+          -- },
+        },
+      },
+    },
   },
 }
